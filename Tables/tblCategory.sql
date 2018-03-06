1ed519ed2b2620e27d410b528eb15edb5fa03192
gfuller16 <graysonfuller16@gmail.com>
2018-03-06 09:08:32 -0500

    Create tblCategory.sql

---
+++ Tables/tblCategory.sql
@@ -0,0 +1,19 @@
+USE [FullerTV]
+GO
+
+/****** Object:  Table [dbo].[tblCategory]    Script Date: 3/6/2018 9:08:13 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblCategory](
+	[catID] [int] NOT NULL,
+	[catName] [varchar](20) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[catID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
